2fc206eeccfc6b5a5f033b682dd5bfbaadd3479e
pinsily <13160724868@163.com>
2018-06-08 17:38:20 +0800

    修改链接blog为blogs，防止后台混淆

--- TuClown/urls.py
+++ TuClown/urls.py
@@ -35,7 +35,7 @@ django.setup()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', xadmin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('blogs/', include('blog.urls')),
     # path('ueditor/', include('DjangoUeditor.urls')),
     # path('mdeditor/', include('mdeditor.urls')),
     # path('markdown/', include( 'django_markdown.urls')),
