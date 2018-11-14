925e13d5ef7ffcec258ceaa9bed3e31334c0af1b
pinsily <13160724868@163.com>
2018-11-14 14:20:20 +0800

    rss 接口 解析markdown

--- blog/feeds.py
+++ blog/feeds.py
@@ -7,7 +7,7 @@
    Description :   rss 订阅功能
 -------------------------------------------------
 """
-
+import markdown
 from django.contrib.syndication.views import Feed
 
 from . import models
@@ -22,10 +22,17 @@ class AllArticalRssFeed(Feed):
         return models.Article.objects.all()
 
     def item_title(self, item):
-        return "[{0}] - {1}".format(item.category.name, item.title)
+        return "{1}".format(item.title)
 
     def item_description(self, item):
-        return item.body
+        # markdown to html
+        extensions = ['markdown.extensions.extra',
+                      'markdown.extensions.codehilite',
+                      'markdown.extensions.tables',
+                      'markdown.extensions.toc'
+                      ]
+
+        return markdown.markdown(item.body, extensions=extensions)
 
 
 if __name__ == '__main__':
