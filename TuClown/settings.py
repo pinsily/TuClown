61a786ed6d3cfb3e5bc35124ef98d11a73e43850
pinsily <13160724868@163.com>
2018-11-14 17:07:19 +0800

    语言切换

--- TuClown/settings.py
+++ TuClown/settings.py
@@ -100,7 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans'
+# LANGUAGE_CODE = 'zh-Hans'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Asia/Shanghai'
 
