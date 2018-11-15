7cc7da597aac1bc449246effb01b909eddfa8c52
pinsily <13160724868@163.com>
2018-11-15 10:15:07 +0800

    使用回自带后台admin

--- TuClown/urls.py
+++ TuClown/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -27,7 +28,8 @@ sitemaps = {
 }
 
 urlpatterns = [
-    path('admin/', xadmin.site.urls, name="admin"),
+    path('xadmin/', xadmin.site.urls, name="xadmin"),
+    path('admin/', admin.site.urls, name="admin"),
     path('', include('blog.urls')),
     path('comment/', include('comment.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
--- blog/admin.py
+++ blog/admin.py
@@ -3,8 +3,32 @@ from django.contrib import admin
 # Register your models here.
 from .models import Article, Category, Tag, Link, LinkCategory
 
-admin.site.register(Article)
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Link)
-admin.site.register(LinkCategory)
+
+class ArticleAdmin(admin.ModelAdmin):
+    # fields = ['title', 'created_time', 'status', 'category', 'tags']
+    list_display = ['title', 'created_time', 'status']
+    list_filter = ['created_time', 'status']
+    date_hierarchy = 'created_time'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_time']
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_time']
+
+
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ['name', 'url']
+
+
+class LinkCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Link, LinkAdmin)
+admin.site.register(LinkCategory, LinkCategoryAdmin)
